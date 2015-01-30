15d047753e3c50155b958e36b4515131f1e7670c
Takuya ASADA <syuu@cloudius-systems.com>
2015-01-31 08:39:39 +0900

    Add Capstanfile

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+rm -rf ROOTFS usr.manifest x86_64-default-osvapp-gcc
+make install T=x86_64-default-osvapp-gcc OSV_SDK=`readlink -f ../..`
+mkdir -p ROOTFS
+cp -a x86_64-default-osvapp-gcc/app/* ROOTFS/
+for i in ROOTFS/*
+do echo /${i#ROOTFS/}: \${MODULE_DIR}/$i
+done > usr.manifest
