b8c4c01f18fa66162133c8147882fe68d5fc8c25
Takuya ASADA <syuu@cloudius-systems.com>
2015-02-25 08:13:52 +0900

    build.sh: switch to native, since AVX patchset merged

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-rm -rf ROOTFS usr.manifest x86_64-default-osvapp-gcc examples/*/build/*
-make install T=x86_64-default-osvapp-gcc OSV_SDK=`readlink -f ../..` || exit $?
-(cd examples/cmdline; env RTE_SDK=`readlink -f ../../` RTE_TARGET=x86_64-default-osvapp-gcc make)
-(cd examples/helloworld; env RTE_SDK=`readlink -f ../../` RTE_TARGET=x86_64-default-osvapp-gcc make)
-(cd examples/ip_fragmentation; env RTE_SDK=`readlink -f ../../` RTE_TARGET=x86_64-default-osvapp-gcc make)
-(cd examples/l2fwd; env RTE_SDK=`readlink -f ../../` RTE_TARGET=x86_64-default-osvapp-gcc make)
-(cd examples/l3fwd; env RTE_SDK=`readlink -f ../../` RTE_TARGET=x86_64-default-osvapp-gcc make)
+rm -rf ROOTFS usr.manifest x86_64-native-osvapp-gcc examples/*/build/*
+make install T=x86_64-native-osvapp-gcc OSV_SDK=`readlink -f ../..` || exit $?
+(cd examples/cmdline; env RTE_SDK=`readlink -f ../../` RTE_TARGET=x86_64-native-osvapp-gcc make)
+(cd examples/helloworld; env RTE_SDK=`readlink -f ../../` RTE_TARGET=x86_64-native-osvapp-gcc make)
+(cd examples/ip_fragmentation; env RTE_SDK=`readlink -f ../../` RTE_TARGET=x86_64-native-osvapp-gcc make)
+(cd examples/l2fwd; env RTE_SDK=`readlink -f ../../` RTE_TARGET=x86_64-native-osvapp-gcc make)
+(cd examples/l3fwd; env RTE_SDK=`readlink -f ../../` RTE_TARGET=x86_64-native-osvapp-gcc make)
 mkdir -p ROOTFS
-find x86_64-default-osvapp-gcc/app/ -executable -readable -type f -exec cp -v {} ROOTFS/ \;
+find x86_64-native-osvapp-gcc/app/ -executable -readable -type f -exec cp -v {} ROOTFS/ \;
 find examples/*/build/app -executable -readable -type f -exec cp -v {} ROOTFS/ \;
 for i in ROOTFS/*
 do echo /${i#ROOTFS/}: \${MODULE_DIR}/$i
