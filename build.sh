fc30f3b4bf95a406f9d5550dff19554ed16f7801
Takuya ASADA <syuu@cloudius-systems.com>
2015-02-25 07:39:13 +0900

    build.sh: build some examples

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-rm -rf ROOTFS usr.manifest x86_64-default-osvapp-gcc
+rm -rf ROOTFS usr.manifest x86_64-default-osvapp-gcc examples/*/build/*
 make install T=x86_64-default-osvapp-gcc OSV_SDK=`readlink -f ../..` || exit $?
+(cd examples/cmdline; env RTE_SDK=`readlink -f ../../` RTE_TARGET=x86_64-default-osvapp-gcc make)
+(cd examples/helloworld; env RTE_SDK=`readlink -f ../../` RTE_TARGET=x86_64-default-osvapp-gcc make)
+(cd examples/ip_fragmentation; env RTE_SDK=`readlink -f ../../` RTE_TARGET=x86_64-default-osvapp-gcc make)
+(cd examples/l2fwd; env RTE_SDK=`readlink -f ../../` RTE_TARGET=x86_64-default-osvapp-gcc make)
+(cd examples/l3fwd; env RTE_SDK=`readlink -f ../../` RTE_TARGET=x86_64-default-osvapp-gcc make)
 mkdir -p ROOTFS
-cp -a x86_64-default-osvapp-gcc/app/* ROOTFS/
+find x86_64-default-osvapp-gcc/app/ -executable -readable -type f -exec cp -v {} ROOTFS/ \;
+find examples/*/build/app -executable -readable -type f -exec cp -v {} ROOTFS/ \;
 for i in ROOTFS/*
 do echo /${i#ROOTFS/}: \${MODULE_DIR}/$i
 done > usr.manifest
